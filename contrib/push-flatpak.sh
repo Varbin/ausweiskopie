e155bad04592c50558667abc81acf857f0c33bfb
Simon Biewald <simon@fam-biewald.de>
2025-03-13 03:59:58 +0100

    Create .ssh if it does not exist...

--- contrib/push-flatpak.sh
+++ contrib/push-flatpak.sh
@@ -7,6 +7,7 @@ outdir="./flathub"
 outpush="git@github.com:varbin/in.varb.Ausweiskopie.git"
 
 if [ -v FLATPAK_UPDATE_KEY ]; then
+  mkdir -p ~/.ssh ; true
   echo "$FLATPAK_UPDATE_KEY" >> ~/.ssh/id_flatpak
   chmod 600 ~/.ssh/id_flatpak
   export GIT_SSH_COMMAND='ssh -i ~/.ssh/id_flatpak -o IdentitiesOnly=yes'
