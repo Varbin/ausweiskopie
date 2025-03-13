32d58f478b70d0baf6d9696a806bdfdf5889ac2b
Simon Biewald <simon@fam-biewald.de>
2025-03-13 03:51:15 +0100

    Fix ssh key filename and permissions.

--- contrib/push-flatpak.sh
+++ contrib/push-flatpak.sh
@@ -7,7 +7,8 @@ outdir="./flathub"
 outpush="git@github.com:varbin/in.varb.Ausweiskopie.git"
 
 if [ -v FLATPAK_UPDATE_KEY ]; then
-  echo "$FLATPAK_UPDATE_KEY" >> ~/id_flatpak
+  echo "$FLATPAK_UPDATE_KEY" >> ~/.ssh/id_flatpak
+  chmod 600 ~/.ssh/id_flatpak
   export GIT_SSH_COMMAND='ssh -i ~/.ssh/id_flatpak -o IdentitiesOnly=yes'
 fi
 
